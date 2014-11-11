86d7c796f2e7ec2e682bb7708aee52f8c88bd995
Christopher Davies <chris.davies@sparcedge.com>
2014-11-10 22:10:41 -0500

    Initializing inputs

--- js/primepattern.js
+++ js/primepattern.js
@@ -1,1 +1,36 @@
-﻿
+﻿(function initializeDatasetDropdown() {
+
+    function numericSuffix(i) {
+        var suffix = 'th';
+
+        if (i < 4 || i > 20) {
+            var mod = i % 10;
+            if (mod == 1) {
+                suffix = 'st';
+            } else if (mod == 2) {
+                suffix = 'nd';
+            } else if (mod == 3) {
+                suffix = 'rd';
+            }
+        }
+
+        return suffix;
+    }
+
+    function makeOption(i) {
+        return '<option value="' + i + '">' + i + numericSuffix(i) + '</option>';
+    }
+
+    function initializeDropdown() {
+        var options = [],
+            i = 0;
+
+        while (options.length < 50) {
+            options.push(makeOption(++i));
+        }
+
+        $('.dataset-dropdown').html(options.join(''));
+    }
+
+    initializeDropdown();
+})();
